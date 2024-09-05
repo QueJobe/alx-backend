2df7f743956be45f880c0a91feacc4b43a08a1af
Blessing <bqdithole22@live.com>
2024-09-05 05:49:27 +0200

    basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""BasicCache Module"""
-
+""" Basci cache module """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache class"""
-
+    """ Basic cache class """
     def put(self, key, item):
         """ Add an item in the cache """
-
         if key is None or item is None:
             pass
         else:
@@ -17,5 +14,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Get an item by key """
-
         return self.cache_data.get(key)
